6dc31c8c39f975700373802342ef42adfd8b5e86
Pyish <kip5ishmael@gmail.com>
2021-06-11 02:44:51 +0300

    final

--- public/js/waypoints.js
+++ public/js/waypoints.js
@@ -1,7 +1,7 @@
 /*
 jQuery Waypoints - v1.1.7
 Copyright (c) 2011-2012 Caleb Troughton
-Dual licensed under the MIT license and GPL license.
+Dual licensed under the license and GPL license.
 https://github.com/imakewebthings/jquery-waypoints/blob/master/MIT-license.txt
 https://github.com/imakewebthings/jquery-waypoints/blob/master/GPL-license.txt
 */
